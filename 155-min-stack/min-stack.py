16dd18c15ccef2deabe993e08cb300694a58425d
Harshita Ravindran Revathi <66433624+harshitarr@users.noreply.github.com>
2026-06-20 21:03:55 +0530

    Time: 203 ms (12.30%) | Memory: 24.9 MB (9.61%) - LeetSync

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,25 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack=[]
+        self.minStack=[]
+        
+
+    def push(self, val):
+        self.stack.append(val)
+
+        if not self.minStack or val <= self.minStack[-1]:
+            self.minStack.append(val)
+
+
+    def pop(self):
+        removed = self.stack.pop()
+
+        if removed == self.minStack[-1]:
+            self.minStack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.minStack[-1]
