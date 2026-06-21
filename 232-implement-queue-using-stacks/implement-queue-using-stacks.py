63a9fe8c12858399267ff41f3ae271f726c63953
Harshita Ravindran Revathi <66433624+harshitarr@users.noreply.github.com>
2026-06-21 22:45:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (62.27%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -0,0 +1,25 @@
+class MyQueue(object):
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x):
+        self.s1.append(x)
+
+    def transfer(self):
+        while self.s1:
+            self.s2.append(self.s1.pop())
+
+    def pop(self):
+        if not self.s2:
+            self.transfer()
+        return self.s2.pop()
+
+    def peek(self):
+        if not self.s2:
+            self.transfer()
+        return self.s2[-1]
+
+    def empty(self):
+        return len(self.s1) == 0 and len(self.s2) == 0
