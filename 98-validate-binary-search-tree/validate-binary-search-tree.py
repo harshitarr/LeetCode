d4bc8d3ad459ed8ff96419880e8c0a9d6e4a08d0
Harshita Ravindran Revathi <66433624+harshitarr@users.noreply.github.com>
2026-07-11 22:20:05 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (99.74%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -4,31 +4,26 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution(object):
     def isValidBST(self, root):
-
         stack = []
         curr = root
         prev = None
 
         while curr or stack:
 
-            # Go to the leftmost node
             while curr:
                 stack.append(curr)
                 curr = curr.left
-
-            # Visit the node
+                
             curr = stack.pop()
 
-            # Check BST property
-            if prev is not None and curr.val <= prev:
+
+            if prev is not None and curr.val<=prev:
                 return False
 
             prev = curr.val
 
-            # Move to the right subtree
             curr = curr.right
-
         return True
+        
