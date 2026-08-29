e00addef3e00f75cbbb44431f8fa892e6414db8e
Harshita Ravindran Revathi <66433624+harshitarr@users.noreply.github.com>
2026-08-29 12:34:17 +0530

    Time: 3 ms (90.29%) | Memory: 13.5 MB (49.92%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,15 +1,11 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        # step 1 : Move the non Zeros elements in the forward
-        k=0 
+        pos = 0
         for i in range(len(nums)):
             if nums[i]!=0:
-                nums[k]=nums[i]
-                k=k+1
-                
-        #Step 2: Filling the rest with zeros
-        for i in range(k,len(nums)):
-            nums[i]=0
+                nums[pos],nums[i]=nums[i],nums[pos]
+                pos+=1
+        return nums
 
 
 
